ccaee792581891c706e321499c57a8c839561574
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-04-08 14:11:33 +0600

    added

---
+++ script.js
@@ -0,0 +1,85 @@
+// //variables
+// //var
+// var myNumber = 100; //declare
+// var myNumber = 200; //re-declare
+// myNumber = 300; //re-assign
+// console.log(myNumber);
+// //let
+// let friendName = "Tiyara";
+// //const
+// const temp = 28.56;
+
+// //without any keyword
+// test = "error"; //can't create scope
+// console.log(test);
+
+// data types
+//primitive, non-primitive
+//primitive
+//number
+
+// const number = 10;
+// console.log(typeof 10);
+
+//string
+// const libraryName = "react";
+// console.log(typeof libraryName);
+
+//boolean
+// const isRaining = false;
+// console.log(typeof isRaining);
+
+// math oprators(+,-,*,/,%,**)
+
+// const a = 5;
+// const b = 4;
+// const sum = a + b;
+// const diffrence = a - b;
+// const division = a / b;
+// const product = a * b;
+// const mod = a % b;
+// const sq = a ** b; //a^b,10^2
+
+// console.log(sum);
+
+//operator precedence
+//5+4
+//*10
+// /3
+// const result = ((5 + 4) * 10) / 3;
+// console.log(result);
+
+// const a = 10.356;
+// const b = 5.456;
+// const sum = a + b;
+// console.log(Number(sum.toFixed(2)));
+// console.log(+sum.toFixed(3));
+
+// const red = "100px";
+// console.log(parseInt(red));
+
+// comparison operators(<,>,<=,>=)
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5 >= 4);
+// console.log(5 <= 6);
+
+//assignment operators(==,===,!=,!==)
+// console.log(5 == "5"); //only checks value
+// console.log(5 === "5"); //checks both value and type
+// console.log(5 != "6");
+// console.log(5 !== "5");
+
+//type conversion and type coersion
+// Type coercion is the automatic or implicit conversion
+const myNumber = "10";
+// console.log(typeof +myNumber);
+
+//falsy values(null,undefined,false,',0,NaN)
+
+// const myNUm = "";
+// if (myNUm) {
+//   console.log("truthy");
+// } else {
+//   console.log("falsy");
+// }
